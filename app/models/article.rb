868aad8e8445b697f706dbad4acc5154e02485a2
rory jackson <roryjamesjackson@hotmail.com>
2022-12-02 09:29:06 +0000

    seeds finished

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  belongs_to :user
+  has_many :comments
 end
